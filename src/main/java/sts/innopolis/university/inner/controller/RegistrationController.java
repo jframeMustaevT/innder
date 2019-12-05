1f3f541ea8f4b7827d5aa165d3eb55917018272f
mustaevtt <company.abb@gmail.com>
2019-12-05 21:36:44 +0300

    main page fixed

--- src/main/java/sts/innopolis/university/inner/controller/RegistrationController.java
+++ src/main/java/sts/innopolis/university/inner/controller/RegistrationController.java
@@ -3,10 +3,7 @@ package sts.innopolis.university.inner.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import sts.innopolis.university.inner.domain.Registration;
 import sts.innopolis.university.inner.service.UserService;
 
