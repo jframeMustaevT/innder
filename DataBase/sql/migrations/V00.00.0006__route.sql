7a1ba45c90365e560bf1c0ef8778fed4116a27dc
Diw <test@ail.ru>
2019-12-09 18:27:11 +0300

    update data base. Delete old migration

--- DataBase/sql/migrations/V00.00.0006__route.sql
+++
@@ -1,9 +0,0 @@
--- Stories table.
-
-CREATE TABLE route
-(
-    id          BIGSERIAL,
-    start       text NOT NULL,
-    finish      text NULL default Null,
-    CONSTRAINT  route PRIMARY KEY (id)
-);
--- DataBase/sql/migrations/V00.00.0007__companion.sql
+++
@@ -1,9 +0,0 @@
--- Stories table.
-
-CREATE TABLE companion
-(
-    id      BIGSERIAL,
-    user_id BIGSERIAL NOT NULL,
-    trip_id BIGSERIAL NULL default Null,
-    CONSTRAINT companion_pk PRIMARY KEY (user_id, trip_id)
-);
