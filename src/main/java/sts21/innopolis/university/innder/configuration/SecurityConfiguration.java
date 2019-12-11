36b1d5d00d6f639ea8844a323bc910c32addb3fe
mustaevtt <company.abb@gmail.com>
2019-12-11 18:29:45 +0300

    static files added

--- src/main/java/sts21/innopolis/university/innder/configuration/SecurityConfiguration.java
+++ src/main/java/sts21/innopolis/university/innder/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web
+            .ignoring()
+            .antMatchers("/static/**")
+            .antMatchers("/css/**")
+            .antMatchers("/fonts/**")
+            .antMatchers("/images/**")
+            .antMatchers("/js/**")
+            .antMatchers("/vendor/**");
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
