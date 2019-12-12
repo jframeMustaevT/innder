95f900422e5fab16755a4eaebd2d80336c149dc6
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-12-12 14:53:59 +0300

    546 Add telegram bot files

---
+++ TelegramBot/src/main/java/stc21/innopolis/university/TokenStorage.java
@@ -0,0 +1,26 @@
+package stc21.innopolis.university;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "TokenStorage")
+public class TokenStorage {
+    @JacksonXmlProperty(isAttribute = true, localName = "Token")
+    private String token;
+
+    public TokenStorage() {
+    }
+
+    public TokenStorage(String token) {
+        this();
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
