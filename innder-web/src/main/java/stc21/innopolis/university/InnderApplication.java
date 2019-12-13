b3f943b7a5cc5f85668f8619c18f1609ced63f80
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-12-13 16:15:46 +0300

    588 division into three modules

---
+++ innder-web/src/main/java/stc21/innopolis/university/InnderApplication.java
@@ -0,0 +1,13 @@
+package stc21.innopolis.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnderApplication.class, args);
+    }
+
+}
