b6fdec74487fa6a67ef01123276e447d38ed81ad
overkri <48020850+overkri@users.noreply.github.com>
2019-12-20 02:54:04 +0300

    ПОИСК ВО ФРОНТЕ!

--- innder-web/src/main/java/stc21/innopolis/university/controller/TripController.java
+++ innder-web/src/main/java/stc21/innopolis/university/controller/TripController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import stc21.innopolis.university.dto.FoundedTrips;
 import stc21.innopolis.university.dto.RequestedTrips;
+import stc21.innopolis.university.dto.Subscribe;
 import stc21.innopolis.university.entity.Trip;
 import stc21.innopolis.university.entity.TripStatus;
 import stc21.innopolis.university.entity.User;
@@ -49,26 +50,37 @@ public class TripController {
 
     @RequestMapping(value = "/search-trip")
     public ModelAndView searchTrip() {
-        //Форма для поиска
         Map<String, Object> model = new HashMap<>();
         return new ModelAndView("search-trip", model);
     }
-    @RequestMapping(value = "/trips", method = RequestMethod.POST)
-    public ModelAndView getTrips(@RequestBody RequestedTrips requestedTrips){
+
+    @RequestMapping(value = "/trips")
+    public ModelAndView trips() {
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView("trips", model);
+    }
+
+    @RequestMapping(value = "/search-trip", method = RequestMethod.POST)
+    @ResponseBody
+    public Collection<FoundedTrips> getTrips(@RequestBody RequestedTrips requestedTrips){
         Collection<Trip> trips = tripRepository
                 .findByRoute_StartCityAndRoute_EndCityAndStartDataTimeBetween(
-                requestedTrips.getStartCity(),
-                requestedTrips.getEndCity(),
-                requestedTrips.getMinStartDataTime(),
-                requestedTrips.getMaxStartDataTime());
+                        requestedTrips.getStartCity(),
+                        requestedTrips.getEndCity(),
+                        requestedTrips.getMinStartDataTime(),
+                        requestedTrips.getMaxStartDataTime());
         Collection<FoundedTrips> foundedTrips = trips
                 .stream()
                 .map(t -> {return new FoundedTrips(t);})
                 .collect(Collectors.toList());
-        Map<String, Object> model = new HashMap<>();
-        model.put("foundedTrips", foundedTrips);
-        return new ModelAndView("trips", model);
+            return foundedTrips;
     }
 
+    @RequestMapping(value = "/trips", method = RequestMethod.POST)
+    @ResponseBody
+    public Integer subscribe(@RequestBody Subscribe sub) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return sub.getId();
+    }
 }
 
