a7a9e754491bfa3b376d57699ef973c1d02e258a
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-12-19 12:16:34 +0300

    546 Work in progress

--- innder-web/src/main/java/stc21/innopolis/university/controller/ApiController.java
+++ innder-web/src/main/java/stc21/innopolis/university/controller/ApiController.java
@@ -1,12 +1,22 @@
 package stc21.innopolis.university.controller;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import stc21.innopolis.university.interaction.bot.InteractionWithBot;
 
 import javax.transaction.Transactional;
 
 @Controller
-@Transactional
+//@Transactional
+@RequestMapping(value = "/api")
 public class ApiController {
 
-    //private botUrl
+    private InteractionWithBot bot;
+
+    public ApiController(InteractionWithBot bot) {
+        this.bot = bot;
+    }
+
+    public void setUserChatId(){}
 }
