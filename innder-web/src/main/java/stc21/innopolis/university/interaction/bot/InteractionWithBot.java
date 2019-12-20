402ea6ad35e2979980abd4ee0e402664e0d0f5dc
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-12-20 05:45:40 +0300

    546 - add send message to telegram bot

--- innder-web/src/main/java/stc21/innopolis/university/interaction/bot/InteractionWithBot.java
+++ innder-web/src/main/java/stc21/innopolis/university/interaction/bot/InteractionWithBot.java
@@ -3,7 +3,9 @@ package stc21.innopolis.university.interaction.bot;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import stc21.innopolis.innderbot.entity.RequestToBot;
 import stc21.innopolis.innderbot.entity.ResponseOfBot;
 
 @Component
@@ -16,11 +18,17 @@ public class InteractionWithBot {
         this.botUrl = botUrl;
     }
 
-    public boolean sendMessage(String chatId, String message){
+    public boolean sendMessage(String chatId, String message) {
         final RestTemplate restTemplate = new RestTemplate();
-        //final ResponseOfBot =
-        String answer = restTemplate.getForObject(botUrl+send, String.class);
-        System.out.println(answer);
+        final RequestToBot requestToBot = new RequestToBot(chatId, message);
+        try {
+            final ResponseOfBot responseOfBot = restTemplate.postForObject(botUrl + send, requestToBot, ResponseOfBot.class);
+            return responseOfBot.getAnswer() == "OK";
+        }catch (RestClientException e){
+            //todo logger
+            e.printStackTrace();
+            System.out.println("Message to telegram don't send");
+        }
         return false;
     }
 
